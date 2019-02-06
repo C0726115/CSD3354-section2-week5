f6350cca3291dc6cbd485148af9d9053e71a523a
visualstudio <visualstudio@122-PC28>
2019-02-06 12:02:47 -0500

    static

--- C0726115/Program.cs
+++ C0726115/Program.cs
@@ -8,8 +8,30 @@ namespace C0726115
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+                static int i = 0;
+                static void Main(string[] args)
+                {
+                    MethodA();
+                    Console.ReadLine();
+                }
+                public static void MethodA()
+                {
+                    while (peanut())
+                    {
+                        Console.WriteLine("i is {0}", i);
+                        if (i > 10)
+                        {
+                            return;
+                        }
+                    }
+                }
+                public static bool peanut()
+                {
+                    i++;
+                    return true;
+                }
+
+
+        
     }
 }
